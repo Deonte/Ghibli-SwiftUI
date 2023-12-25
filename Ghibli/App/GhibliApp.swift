ddf9a96c027a604ac98ca2e39cfd7d98b64b94d3
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-12-24 22:00:09 -0500

    - Added Data file - Removed SwiftData Stater Files - Project Structure

---
+++ Ghibli/App/GhibliApp.swift
@@ -0,0 +1,18 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Deonte Kilgore on 12/24/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FilmListView()
+        }
+    }
+}
