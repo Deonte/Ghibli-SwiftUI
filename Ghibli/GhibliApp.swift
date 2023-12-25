4b242e214179f8666cf47e7b5458f9a3f0d82630
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-12-24 21:38:20 -0500

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,32 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Deonte Kilgore on 12/24/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GhibliApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
