10f0e6664227def9a71bdfa523f903a588d37b13
Deonte Kilgore <deonte.kilgore.dev@gmail.com>
2023-12-27 22:20:11 -0500

    - Manually added preview to the film model used for previews. - Added Film Cell - Removed Text and change to list of films.

---
+++ Ghibli/Views/FilmCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilmCell.swift
+//  Ghibli
+//
+//  Created by Deonte Kilgore on 12/27/23.
+//
+
+import SwiftUI
+
+struct FilmCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilmCell()
+}
